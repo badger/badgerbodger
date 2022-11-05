461a143fe479ed325233001f8650b6dd89a3d9d0
Martin Woodward <martinwoodward@github.com>
2022-11-05 09:12:30 -0700

    Add script to flash a badge

---
+++ flash.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Wait for keypress
+echo "Press key after Badge in Flash mode to nuke"
+read -n 1 -s
+
+cp uf2-images/flash_nuke.uf2 /Volumes/RPI-RP2
+
+echo "Wait for 2 flashes then press to flash"
+read -n 1 -s
+cp uf2-images/github-badger2040-mona-micropython.uf2 /Volumes/RPI-RP2
+
+echo "Wait for LED on then enter the GitHub handle:"
+read handle
+./bodge.sh $handle
+
+echo "Done!"
+
