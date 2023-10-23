6defb10cf3141aab72652f9bec5efbdfa9aad97f
Martin Woodward <martinwoodward@github.com>
2023-10-24 00:29:53 +0100

    Tidy up flashing

--- scripts/gui/state_page.py
+++ scripts/gui/state_page.py
@@ -10,7 +10,8 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 class StatePage(tk.Frame):
     def __init__(self, parent, state):
         tk.Frame.__init__(self, parent)
+        self.config(background='black')
         imgTk = ImageTk.PhotoImage(Image.open(os.path.join(script_dir,f'images/{state}.png')).resize((480,800)))
-        imgLabel = tk.Label(self,image=imgTk,borderwidth=0,highlightthickness=0)
+        imgLabel = tk.Label(self,image=imgTk,borderwidth=0,highlightthickness=0,background="black")
         imgLabel.image = imgTk
         imgLabel.place(x=0,y=0)
